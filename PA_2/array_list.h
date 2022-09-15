dd2090080d1cf76d9daa3539d11ac0b82bcfda75
Matthew Speights <mattcspeights@gmail.com>
2022-09-15 12:23:26 -0500

    ArrayList

--- PA_2/array_list.h
+++ PA_2/array_list.h
@@ -1,8 +1,155 @@
 #pragma once
+#ifndef ARRAY_LIST_H
+#define ARRAY_LIST_H
+
+#include <stdexcept>
 
 template <typename Object>
 class ArrayList {
-    // TODO(student): implement ArrayList
+    Object* list;
+    size_t list_size;
+    size_t list_capacity;
+
+    public:
+
+        size_t size() const{
+            return list_size;
+        }
+
+        size_t capacity() const{
+            return list_capacity;
+        }
+
+        ArrayList(size_t capacity = 1): list{}, list_size(0), list_capacity(capacity) {
+            list = new Object[capacity];
+        }
+
+        ~ArrayList(){
+            delete[] list;
+        }
+
+        ArrayList(const ArrayList& other): list{}, list_size(0), list_capacity(0){
+            if (other.list != nullptr && other.list_capacity != 0){
+                Object* newList = new Object[other.list_capacity];
+
+                for (size_t i = 0; i < other.list_size; ++i){
+                    newList[i] = other.list[i];
+                }
+
+                list_size = other.list_size;
+                list_capacity = other.list_capacity;
+                list = newList;
+            }
+        }
+
+        ArrayList& operator=(const ArrayList& other){
+            if (this->list == other.list){
+                return *this;
+            }
+
+            if (other.list != nullptr && other.list_capacity != 0){
+                Object* newList = new Object[other.list_capacity];
+
+                for (size_t i = 0; i < other.list_size; ++i){
+                    newList[i] = other.list[i];
+                }
+
+                delete[] list;
+
+                list_size = other.list_size;
+                list_capacity = other.list_capacity;
+                list = newList;
+            }
+
+            return *this;
+        }
+
+        Object& operator[](size_t index){
+            if (index > list_size){
+                throw std::invalid_argument("Index out of bounds");
+            }
+
+            return list[index];
+        }
+
+        void resize(){
+
+            Object* newList = nullptr;
+
+            if (this->is_empty()){
+                newList = new Object[1];
+                this->list_capacity = 1;
+            }
+            else{
+                newList = new Object[this->list_capacity * 2];
+                this->list_capacity = 2 * this->list_capacity;
+            }
+            
+            for (size_t i = 0; i < this->list_size; ++i){
+                newList[i] = this->list[i];
+            }
+
+            delete[] this->list;
+
+            this->list = newList;
+        }
+
+        bool is_empty() const{
+            if (this->list == nullptr){
+                return true;
+            }
+
+            if (this->list_size == 0){
+                return true;
+            }
+
+            return false;
+        }
+
+        void insert(size_t index, const Object& obj){
+            if (index > list_size){
+                throw std::invalid_argument("Index out of bounds");
+            }
+
+            if (this->is_empty() && index == 0){
+                this->resize();
+                this->list[index] = obj;
+            }
+            else if (this->list_size != 0){
+                if (this->list_size + 1 > list_capacity){
+                    this->resize();
+                }
+                for (size_t i = this->list_size; i > index; --i){
+                    Object temp = this->list[i - 1];
+                    this->list[i] = temp;
+                }
+                this->list[index] = obj;
+            }
+
+            this->list_size++;    
+        }
+
+        void remove(size_t index){
+            if (index > list_size){
+                throw std::invalid_argument("Index out of bounds");
+            }
+
+            for(size_t i = 0; i < this->list_size; ++i){
+                if (i == index){
+                    for(size_t j = i; j < this->list_size - 1; ++j){
+                        this->list[j] = this->list[j + 1];
+                    }
+                    this->list_size--;
+                    break;
+                }
+            }
+        }
+
+
+
+
+
+
 };
 
 #endif  // ARRAY_LIST_H
--- PA_2/array_list_tests.cpp
+++ PA_2/array_list_tests.cpp
@@ -2,5 +2,41 @@
 #include "array_list.h"
 
 int main() {
-    // TODO(student): test your code
+    ArrayList<int> test1;
+    ArrayList<int> test3;
+
+    if (test1.is_empty()){
+        std::cout << "Correct Behavior for empty" << std::endl;
+    }
+
+    test1.insert(0, 1);
+    test1.insert(1, 10);
+    test1.insert(2, 20);
+    test1.insert(3, 60);
+    test1.insert(4, 55);
+
+    ArrayList<int> test2(test1);
+
+    test3 = test1;
+
+    try{
+        test1.insert(20, 5);
+        test1.remove(20);
+    }catch(std::invalid_argument& exception){
+        std::cout << "Correct Behavior" << std::endl;
+    }
+
+    try{
+        test1.remove(20);
+    }catch(std::invalid_argument& exception){
+        std::cout << "Correct Behavior" << std::endl;
+    }
+
+    test1.remove(0);
+
+
+    for(size_t i = 0; i < test1.size(); ++i){
+        std::cout << test1[i] << " ";
+    }
+    std::cout << std::endl;
 }
